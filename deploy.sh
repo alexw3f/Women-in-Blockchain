307668543249f012217b8a38f74c6e5710ff9d43
Sven Stucki <git@svenstucki.ch>
2018-06-11 04:59:17 +0200

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+SRV=pc
+
+# zip
 rm wib.zip
 cp build/bundle.js .
 zip -r wib.zip . -x node_modules\* .git\*
+
+# upload
+scp wib.zip $SRV:/var/www/flowers
+
+# unpack
+ssh $SRV "cd /var/www/flowers ; unzip wib.zip"
--- src/scripts/w3.js
+++ src/scripts/w3.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 
-// const w3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/yA4kiAW9mGJ7YldlGZvS'));
-const w3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+const w3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/yA4kiAW9mGJ7YldlGZvS'));
+// const w3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 // wallet
 const addr = '0xaa05994e1236a722904c5f0e572a50008d016cae';
